069d0d7ecb96636a6f760ba21d5059965a38e8ac
Andrew Mason <mason@case.edu>
2015-05-19 22:45:08 -0700

    add some validation specs for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :institution }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  it { should have_secure_password }
 end
