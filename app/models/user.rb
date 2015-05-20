1b6be6e0d64281322dcb7c10fe4569b7119cfd95
Andrew Mason <mason@case.edu>
2015-05-19 23:01:58 -0700

    passing user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   include Clearance::User
+
+  validates :name, :institution, presence: true, allow_blank: false
+  has_secure_password
+
+  # has_secure_password requires a password_digest attribute. So we'll just
+  # use the sha that clearance generates already.
+  alias_attribute :password_digest, :encrypted_password
 end
