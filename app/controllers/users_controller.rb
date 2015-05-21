9153d3c8288c567009d36d5927f7ae76422b3af7
Andrew Mason <mason@case.edu>
2015-05-20 21:38:03 -0700

    fixing style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,7 +72,7 @@ class UsersController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params
-    .require(:user)
-    .permit(:name, :email, :institution, :password, :password_confirmation)
+      .require(:user)
+      .permit(:name, :email, :institution, :password, :password_confirmation)
   end
 end
