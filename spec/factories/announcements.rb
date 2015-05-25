4be9583931f8602c79f0ecab77a199b12020ed6d
Andrew Mason <mason@case.edu>
2015-05-25 11:34:51 -0700

    define the announcement factory

--- spec/factories/announcements.rb
+++ spec/factories/announcements.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
   factory :announcement do
+    title { Forgery::LoremIpsum.sentence }
+    content { Forgery::LoremIpsum.paragraph }
+    association :author, factory: :user
   end
 end
