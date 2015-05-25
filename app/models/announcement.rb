509b12f01f0cf0332216b85022a63f72403323e1
Andrew Mason <mason@case.edu>
2015-05-25 10:19:04 -0700

    passing specs

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,2 +1,5 @@
 class Announcement < ActiveRecord::Base
+  validates :title, :content, presence: true, allow_blank: false
+
+  belongs_to :author, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   # has_secure_password requires a password_digest attribute. So we'll just
   # use the sha that clearance generates already.
   alias_attribute :password_digest, :encrypted_password
+
+  has_many :announcements, foreign_key: :author_id, dependent: :destroy
 end
