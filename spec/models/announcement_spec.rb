ed10b89e2bf34597a576a5c9f2f0b42ec1192c5e
Andrew Mason <mason@case.edu>
2015-05-25 10:04:55 -0700

    validation and association specs

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :content }
+  end
+
+  describe 'associations' do
+    it { should belong_to :author }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :institution }
-  it { should validate_presence_of :email }
-  it { should validate_uniqueness_of :email }
-  it { should have_secure_password }
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :institution }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should have_secure_password }
+  end
+
+  describe 'associations' do
+    it { should have_many(:announcements).dependent(:destroy) }
+  end
 end
