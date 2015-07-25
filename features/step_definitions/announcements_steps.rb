63efe83dce4da894658881bf5e0163a1a741d8fb
Andrew Mason <mason@case.edu>
2015-07-25 16:39:10 -0700

    implement more steps for "index" features

--- features/step_definitions/announcements_steps.rb
+++ features/step_definitions/announcements_steps.rb
@@ -25,7 +25,23 @@ When(/^I visit that announcement's (.*) page$/) do |action|
   visit path
 end
 
+When(/^I click on a random announcement$/) do
+  announcements = all('#announcements tbody tr')
+  row = announcements.sample
+
+  show_link = row.all('td a').first
+  id = show_link[:href].match(/^.*\/(\d+)$/).captures[0]
+  @announcement = Announcement.find id
+
+  show_link.click
+end
+
 Then(/^I should see that announcement$/) do
   expect(page).to have_content @announcement.title
   expect(page).to have_content @announcement.content
+end
+
+Then(/^I should see (\d+) announcements$/) do |count|
+  announcements = all('#announcements tbody tr')
+  expect(announcements.length).to eq count.to_i
 end
